0bc1ffd154aa0cb1100959f60bb9f61c65d904c2
DANIEL SALGUERO <DANIEL SALGUERO@LAPTOP-J5ID0GJ2>
2023-07-22 20:04:17 -0600

    Splsha screennnnn jdjdjjjjjj

--- src/VIsta/Bienvenida.java
+++ src/VIsta/Bienvenida.java
@@ -1,19 +1,36 @@
 
 package VIsta;
 
-import Modelo.PrimerUso;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+
+
 
 public class Bienvenida extends javax.swing.JFrame {
 
     public Bienvenida() {
         initComponents();
+        btnRegistroContinuar.setVisible(false);
+        MostrarBoton();
         
        this.setLocationRelativeTo(this);
       
         
     }
 
+    public void MostrarBoton()
+    {
+        Timer time = new Timer(4000, (ActionEvent e) -> {
+           
+            
+           btnRegistroContinuar.setVisible(true);
+        }); 
+        time.start();
+    }
     
+        
+       
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -22,9 +39,11 @@ public class Bienvenida extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        BtnContinuarRegistro = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
+        kGradientPanel1 = new com.k33ptoo.components.KGradientPanel();
+        kGradientPanel2 = new com.k33ptoo.components.KGradientPanel();
+        btnRegistroContinuar = new com.k33ptoo.components.KButton();
+        jLabel4 = new javax.swing.JLabel();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -42,66 +61,92 @@ public class Bienvenida extends javax.swing.JFrame {
         jLabel2.setText("jLabel2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(880, 600));
         setResizable(false);
 
+        jPanel1.setBackground(new java.awt.Color(51, 51, 51));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        BtnContinuarRegistro.setBackground(new java.awt.Color(55, 25, 124));
-        BtnContinuarRegistro.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                BtnContinuarRegistroMouseClicked(evt);
+        jLabel1.setFont(new java.awt.Font("Bookman Old Style", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("¡Bienvenido a ESESPI!");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 130, -1, -1));
+
+        kGradientPanel1.setkEndColor(new java.awt.Color(110, 59, 255));
+        kGradientPanel1.setkStartColor(new java.awt.Color(110, 59, 255));
+
+        javax.swing.GroupLayout kGradientPanel1Layout = new javax.swing.GroupLayout(kGradientPanel1);
+        kGradientPanel1.setLayout(kGradientPanel1Layout);
+        kGradientPanel1Layout.setHorizontalGroup(
+            kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 640, Short.MAX_VALUE)
+        );
+        kGradientPanel1Layout.setVerticalGroup(
+            kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jPanel1.add(kGradientPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-10, 0, 640, 40));
+
+        kGradientPanel2.setkEndColor(new java.awt.Color(110, 59, 255));
+        kGradientPanel2.setkStartColor(new java.awt.Color(110, 59, 255));
+
+        javax.swing.GroupLayout kGradientPanel2Layout = new javax.swing.GroupLayout(kGradientPanel2);
+        kGradientPanel2.setLayout(kGradientPanel2Layout);
+        kGradientPanel2Layout.setHorizontalGroup(
+            kGradientPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 640, Short.MAX_VALUE)
+        );
+        kGradientPanel2Layout.setVerticalGroup(
+            kGradientPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 40, Short.MAX_VALUE)
+        );
+
+        jPanel1.add(kGradientPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(-10, 340, 640, 40));
+
+        btnRegistroContinuar.setBackground(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setText("Registrarme");
+        btnRegistroContinuar.setFont(new java.awt.Font("Bookman Old Style", 1, 18)); // NOI18N
+        btnRegistroContinuar.setkBackGroundColor(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setkEndColor(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setkForeGround(new java.awt.Color(0, 0, 102));
+        btnRegistroContinuar.setkHoverEndColor(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setkHoverForeGround(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setkHoverStartColor(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setkIndicatorColor(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setkPressedColor(new java.awt.Color(255, 102, 0));
+        btnRegistroContinuar.setkSelectedColor(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.setkStartColor(new java.awt.Color(243, 167, 18));
+        btnRegistroContinuar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistroContinuarActionPerformed(evt);
             }
         });
+        jPanel1.add(btnRegistroContinuar, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 220, -1, -1));
 
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("Continuar");
-
-        javax.swing.GroupLayout BtnContinuarRegistroLayout = new javax.swing.GroupLayout(BtnContinuarRegistro);
-        BtnContinuarRegistro.setLayout(BtnContinuarRegistroLayout);
-        BtnContinuarRegistroLayout.setHorizontalGroup(
-            BtnContinuarRegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(BtnContinuarRegistroLayout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addComponent(jLabel3)
-                .addContainerGap(34, Short.MAX_VALUE))
-        );
-        BtnContinuarRegistroLayout.setVerticalGroup(
-            BtnContinuarRegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(BtnContinuarRegistroLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel3)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        jPanel1.add(BtnContinuarRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 480, -1, 30));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VIsta/imagenes/Bienve.png"))); // NOI18N
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VIsta/imagenes/Group 9.png"))); // NOI18N
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 140, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 627, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 379, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void BtnContinuarRegistroMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnContinuarRegistroMouseClicked
+    private void btnRegistroContinuarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistroContinuarActionPerformed
         // TODO add your handling code here:
-        this.setVisible(false);
-        Registro v = new Registro();
-        v.setVisible(true);
-          this.dispose();
-    }//GEN-LAST:event_BtnContinuarRegistroMouseClicked
+    }//GEN-LAST:event_btnRegistroContinuarActionPerformed
 
     
     public static void main(String args[]) {
@@ -137,12 +182,14 @@ public class Bienvenida extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel BtnContinuarRegistro;
+    private com.k33ptoo.components.KButton btnRegistroContinuar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private com.k33ptoo.components.KGradientPanel kGradientPanel1;
+    private com.k33ptoo.components.KGradientPanel kGradientPanel2;
     // End of variables declaration//GEN-END:variables
 }
